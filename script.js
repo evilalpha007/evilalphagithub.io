027c7c17766d28e0f098d47df1dcb095ccb1a23c
evilalpha007 <85012443+evilalpha007@users.noreply.github.com>
2021-12-15 17:50:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer",  "Designer", "singer"],
+        strings: [ "Developer",  "Designer", "coder","writter"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
